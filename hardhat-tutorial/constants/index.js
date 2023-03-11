e7ab3b70ca7534da60b9f786a49bd6408aae46c6
Tirth Shah <tirthshah100@gmail.com>
2023-03-11 17:51:54 +0530

    updated api support

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x96F42f7598786b70A12991D4dD95101c5266B875";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-sigma-amber.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
